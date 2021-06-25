0337a4d7e6137a8fe6a191dd4c054ccb4718977c
Felipe Costa <felipemdb5@gmail.com>
2021-06-25 16:19:43 -0300

    update

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -1,13 +1,24 @@
+const Database = require('../db/config')//importando o banco de dados
+
+
 module.exports = {
-    create(req,res){
+    async create(req,res){
+
+        const db =  await Database()
+        const pass = req.body.password//pegando o elemento com o name = "password" dentro da body da pagina que fez a requisição
 
         let roomId = 0
 
         for(var i = 0;i<6; i++){
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : roomId += Math.floor(Math.random() * 10).toString()  
+            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : roomId += Math.floor(Math.random() * 10).toString()  //o comando Math.floor(Math.random() * 10) gera um numero randômico entre 0 e 10
+            // o comando a cima esta atribuindo um valor randomico a i caso ele seja 0, e caso não seja ele concatena outro valor randômico ao i até gerar um dígito randômico de tamanho 6
             
 
         }
+
+        await db.run(`INSERT INTO rooms(id,pass) VALUES (${parseInt(roomId)}, ${pass})`)
+
+        await db.close()
         
         res.redirect(`/room/${roomId}`)
 
--- src/route.js
+++ src/route.js
@@ -4,13 +4,13 @@ const roomController = require('./controllers/roomController')
 
 const route = express.Router()
 
-route.get('/', (req, res)=> res.render("index",{page: 'enter-room'}))
-route.get('/create-pass', (req,res)=> res.render("index", {page: 'create-pass'}))
+route.get('/', (req, res)=> res.render("index",{page: 'enter-room'}))//renderiza a index com a variável page contendo o pedaço de código HTML enter-room.js
+route.get('/create-pass', (req,res)=> res.render("index", {page: 'create-pass'}))//renderiza a index com a variável page contendo o pedaço de código HTML create-pass.js
 
 route.get('/room/:room', (req,res) => res.render("room"))
 
 
-//Rota da ação a ser executada após os eventos da modal
+//Rota da ação a ser executada após os eventos da modal => Marcar como lida ou Excluir 
 route.post('/question/:varRoom/:questionId/:action', questionController.index)// quando se usa dois pontos ':' se indica uma variável
 route.post('/create-room', roomController.create)
 
